63213151d6135acd840426d78df5f0020c9708cf
Degala Venkata Teja <23pa1a4227@vishnu.edu.in>
2026-07-13 20:09:40 +0530

    Time: 0 ms (100%), Space: 45 MB (79.64%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long low, long high) {
+        if (node == null) return true;
+
+        if (node.val <= low || node.val >= high) return false;
+
+        return validate(node.left, low, node.val) &&
+               validate(node.right, node.val, high);
+    }
+}
