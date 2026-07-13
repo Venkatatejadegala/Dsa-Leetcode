af942e30c8efc4840b5b0c6206675d66ede92eea
Degala Venkata Teja <23pa1a4227@vishnu.edu.in>
2026-07-13 20:17:02 +0530

    Time: 0 ms (100%), Space: 46.2 MB (87.46%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        Node levelStart = root;
+
+        while (levelStart.left != null) {
+            Node curr = levelStart;
+
+            while (curr != null) {
+                curr.left.next = curr.right;
+
+                if (curr.next != null) {
+                    curr.right.next = curr.next.left;
+                }
+
+                curr = curr.next;
+            }
+
+            levelStart = levelStart.left;
+        }
+
+        return root;
+    }
+}
