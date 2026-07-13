3fdeaae4f20d4dda2db7fc44c1963ec0c0f70f53
Degala Venkata Teja <23pa1a4227@vishnu.edu.in>
2026-07-13 10:06:58 +0530

    Time: 0 ms (100%), Space: 42.9 MB (74.31%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) return;
+
+        dfs(node.right);
+        sum += node.val;
+        node.val = sum;
+        dfs(node.left);
+    }
+}
