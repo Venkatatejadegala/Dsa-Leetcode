ca63004d20ec0da996a662c3a0a38ea5d6376fff
Degala Venkata Teja <23pa1a4227@vishnu.edu.in>
2026-07-15 14:32:40 +0530

    Time: 16 ms (20.46%), Space: 52.5 MB (85.64%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int n = nums.length;
+        int limit = n / 2;
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            if (freq.get(num) > limit) {
+                return num;
+            }
+        }
+        return -1; 
+    }
+}
